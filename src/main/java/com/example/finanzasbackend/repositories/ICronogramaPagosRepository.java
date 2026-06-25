4cc68e4c3818b7b28f870a7baa4d85008780984f
Gerardo Huaman <u202313990@upc.edu.pe>
2026-06-25 00:13:53 -0500

    arreglando el repository

--- src/main/java/com/example/finanzasbackend/repositories/ICronogramaPagosRepository.java
+++ src/main/java/com/example/finanzasbackend/repositories/ICronogramaPagosRepository.java
@@ -2,11 +2,14 @@ package com.example.finanzasbackend.repositories;
 
 import com.example.finanzasbackend.entities.CronogramaPagos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ICronogramaPagosRepository extends JpaRepository<CronogramaPagos, Integer> {
-    List<CronogramaPagos> findBySimulacionIdSimulacion(int idSimulacion);
+    @Query(value = "select c from CronogramaPagos c where c.simulacion.id_simulacion = :idSimulacion")
+    List<CronogramaPagos> findBySimulacionIdSimulacion(@Param("idSimulacion") int idSimulacion);
 }
